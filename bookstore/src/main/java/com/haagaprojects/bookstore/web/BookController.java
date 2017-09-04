b122019943a2ff9595ca67b11bd273ee10b9b29c
Daryoush <derpieegaming@gmail.com>
2017-09-04 20:16:07 +0300

    Initial project initiation

---
+++ bookstore/src/main/java/com/haagaprojects/bookstore/web/BookController.java
@@ -0,0 +1,21 @@
+package com.haagaprojects.bookstore.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class BookController {
+	@RequestMapping(value="/index")
+	public String IndexBookStore(){
+	return "index";
+	}
+}
