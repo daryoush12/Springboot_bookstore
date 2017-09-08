cd5e7427b9ae1c1600cfedae537808e39a06a7b4
Daryoush <daryoush_farsimadan@hotmail.com>
2017-09-08 13:08:41 +0300

    Books can now be added and viewed

--- bookstore/src/main/java/com/haagaprojects/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/haagaprojects/bookstore/web/BookController.java
@@ -3,19 +3,46 @@ package com.haagaprojects.bookstore.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.haagaprojects.bookstore.domain.Book;
+import com.haagaprojects.bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
-	@RequestMapping(value="/index")
-	public String IndexBookStore(){
-	return "index";
+	@Autowired
+	private BookRepository Brepository;
+	
+	@RequestMapping(value="/book",method=RequestMethod.GET)
+	public String IndexBookStore(Model model) {
+		model.addAttribute("books", Brepository.findAll());
+		return "book";
 	}
+	@RequestMapping(value="/add")
+	public String addBook(Model model){
+		model.addAttribute("book",new Book());
+		return "add";
+	}
+	@RequestMapping(value="save",method=RequestMethod.POST)
+	public String save(Book book){
+		Brepository.save(book);
+		return "redirect:book";
+	}
+	@RequestMapping(value="/delete/{id}",method=RequestMethod.GET)
+	public String delete(@PathVariable("id") Long id,Model model){
+		System.out.println("DELETE ACTION CALLED: BOOKID:"+id);
+		Brepository.delete(id);
+		return "redirect:book";
+		
+	}
+	
 }
